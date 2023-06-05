f47b612c12f03e092e4c0341069de57b3585faa0
giorgia-nadizar <giorgia.nadizar@gmail.com>
2023-06-05 12:04:47 +0000

    Yaml parsing fixed.

--- cgpax/__init__.py
+++ cgpax/__init__.py
@@ -1,5 +1,14 @@
+from pathlib import Path
+
 import yaml as yaml
 
 
 def get_config(filename: str) -> dict:
-    return yaml.safe_load(filename)
+    config = yaml.safe_load(Path(filename).read_text())
+    flat_config = {}
+    for key in config:
+        if type(config[key]) == dict and "value" in config[key]:
+            flat_config[key] = config[key]["value"]
+        else:
+            flat_config[key] = config[key]
+    return flat_config
