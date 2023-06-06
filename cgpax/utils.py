0f2d547c3d069c1e0cc4b36c13b692cc4e1425d8
giorgia-nadizar <giorgia.nadizar@gmail.com>
2023-06-06 13:45:44 +0000

    Graph viz updated.

--- cgpax/utils.py
+++ cgpax/utils.py
@@ -3,6 +3,8 @@ import jax.numpy as jnp
 
 from cgpax.jax_functions import JaxFunction
 
+import pygraphviz as pgv
+
 
 def compute_active(x_genes: jnp.ndarray, y_genes: jnp.ndarray, f_genes: jnp.ndarray, out_genes: jnp.ndarray,
                    config: dict) -> np.ndarray:
@@ -19,13 +21,12 @@ def __compute_active__(active, x_genes, y_genes, f_genes, n_in, idx):
     if not active[idx]:
         active[idx] = True
         __compute_active__(active, x_genes, y_genes, f_genes, n_in, x_genes[idx - n_in])
-        # TODO check index of f_genes
         _, arity = list(JaxFunction.arities.items())[f_genes[idx - n_in]]
         if arity > 1:
             __compute_active__(active, x_genes, y_genes, f_genes, n_in, y_genes[idx - n_in])
 
 
-def readable_program_from_genome(genome: jnp.ndarray, config: dict):
+def readable_cgp_program_from_genome(genome: jnp.ndarray, config: dict):
     n_in = config["n_in"]
     n_nodes = config["n_nodes"]
     x_genes, y_genes, f_genes, out_genes = jnp.split(genome, jnp.asarray([n_nodes, 2 * n_nodes, 3 * n_nodes]))
@@ -47,3 +48,48 @@ def readable_program_from_genome(genome: jnp.ndarray, config: dict):
     # output selection
     text_function += f"  outputs = buffer[{out_genes}]\n"
     return text_function
+
+
+def readable_lgp_program_from_genome(genome: jnp.ndarray, config: dict):
+    lhs_genes, x_genes, y_genes, f_genes = jnp.split(genome, 4)
+    function_names = list(JaxFunction.existing_functions.keys())
+    text_function = f"def program(inputs, r):\n" \
+                    f"  r[{list(range(config['n_in']))}] = inputs\n"
+
+    # execution
+    for row_idx in range(config["n_rows"]):
+        function_name = function_names[f_genes[row_idx]]
+        text_function += f"  r[{lhs_genes[row_idx]}] = {function_name}(r[{x_genes[row_idx]}]"
+        if JaxFunction.arities[function_name] > 1:
+            text_function += f", r[{y_genes[row_idx]}]"
+        text_function += ")\n"
+
+    # output selection
+    text_function += f"  outputs = r[:{config['n_out']}]\n"
+    return text_function
+
+
+def graph_from_genome(genome: jnp.ndarray, config: dict):
+    n_in = config["n_in"]
+    n_nodes = config["n_nodes"]
+    x_genes, y_genes, f_genes, out_genes = jnp.split(genome, jnp.asarray([n_nodes, 2 * n_nodes, 3 * n_nodes]))
+    active = compute_active(x_genes, y_genes, f_genes, out_genes, config)
+    function_names = list(JaxFunction.existing_functions.keys())
+
+    graph = pgv.AGraph(directed=True)
+    node_ids = [f"i_{n}" for n in range(n_in)]
+    for f_id, fx in enumerate(f_genes):
+        node_ids.append(f"{function_names[fx]}_{f_id + n_in}")
+    for out_id, out_gene in enumerate(out_genes):
+        graph.add_edge(node_ids[out_gene], f"o_{out_id}")
+
+    for buffer_idx in range(n_in, len(active)):
+        if active[buffer_idx]:
+            idx = buffer_idx - n_in
+            current_node = node_ids[buffer_idx]
+            graph.add_edge(node_ids[x_genes[idx]], current_node)
+            function_name = current_node.split("_")[0]
+            if JaxFunction.arities[function_name] > 1:
+                graph.add_edge(node_ids[y_genes[idx]], current_node)
+
+    return graph
--- genome_analysis.py
+++ genome_analysis.py
@@ -8,17 +8,29 @@ import cgpax.jax_encoding
 
 from jax import jit
 
-from cgpax.utils import readable_program_from_genome
+from cgpax.utils import readable_cgp_program_from_genome, graph_from_genome
 from cgpax.run_utils import __update_config_with_env_data__
 
 
 def __write_readable_program__(genome: jnp.ndarray, config: dict, target_file: str = None):
-    readable_program = readable_program_from_genome(genome, config)
+    readable_program = readable_cgp_program_from_genome(genome, config)
     if target_file is None:
         print(readable_program)
     else:
         with open(target_file, "w") as f:
             f.write(readable_program)
+
+
+def __save_graph__(genome: jnp.ndarray, config: dict, file: str, input_color: str = None, output_color: str = None):
+    graph = graph_from_genome(genome, config)
+    graph.layout()
+    if input_color is not None or output_color is not None:
+        for node in graph.iternodes():
+            if node.startswith("i_") and input_color is not None:
+                node.attr["color"] = input_color
+            elif node.startswith("o_") and output_color is not None:
+                node.attr["color"] = output_color
+        graph.draw(file)
 
 
 def __save_html_visualization__(genome: jnp.ndarray, config: dict, file_prefix: str = None):
@@ -31,7 +43,7 @@ def __save_html_visualization__(genome: jnp.ndarray, config: dict, file_prefix: 
         f_initial.write(html.render(env.sys, [state.pipeline_state]))
 
     # full episode visualization
-    program = cgpax.jax_encoding.genome_to_program(genome, config)
+    program = cgpax.jax_encoding.genome_to_cgp_program(genome, config)
     jit_env_reset = jit(env.reset)
     jit_env_step = jit(env.step)
     jit_program = jit(program)
@@ -53,10 +65,12 @@ def __save_html_visualization__(genome: jnp.ndarray, config: dict, file_prefix: 
 
 
 analysis_config = {
-    "run_id": "20230530_125820-42jdh5yc",
+    "run_id": "20230605_074314-135uqvpz",
     "seed": 0,
-    "generation": 99,
+    "generation": 2399,
     "program_file": None,
+    "graph_file": "prova.png",
+    "save_visualization": False
 }
 
 if __name__ == '__main__':
@@ -64,11 +78,16 @@ if __name__ == '__main__':
 
     config = cgpax.get_config(f"{base_path}/config.yaml")
     genome = jnp.load(f"{base_path}/genomes/{analysis_config['seed']}_{analysis_config['generation']}_best_genome.npy",
-                      allow_pickle=True)
+                      allow_pickle=True).astype(int)
 
     env = envs.get_environment(env_name=config["problem"]["environment"], backend=config["backend"])
     env = EpisodeWrapper(env, episode_length=config["problem"]["episode_length"], action_repeat=1)
     __update_config_with_env_data__(config, env)
 
     __write_readable_program__(genome, config, analysis_config['program_file'])
-    reward = __save_html_visualization__(genome, config)
+    if analysis_config["graph_file"] is not None:
+        __save_graph__(genome, config, analysis_config["graph_file"], "green", "red")
+
+    if analysis_config["save_visualization"]:
+        replay_reward = __save_html_visualization__(genome, config)
+        print(f"\nTotal reward = {replay_reward}")
