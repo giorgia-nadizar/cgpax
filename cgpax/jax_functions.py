e5052df93154be25b17972b4e482d150e028125b
giorgia-nadizar <giorgia.nadizar@gmail.com>
2023-06-15 12:41:16 +0000

    Functions cleanup.

--- cgpax/jax_functions.py
+++ cgpax/jax_functions.py
@@ -1,4 +1,3 @@
-
 from __future__ import annotations
 
 from jax import jit
@@ -7,6 +6,32 @@ from functools import partial
 
 import jax.numpy as jnp
 from jax.lax import cond, switch
+
+
+@register_pytree_node_class
+class JaxFunction:
+
+    def __init__(self, op, arity: int, symbol: str) -> None:
+        self.operator = op
+        self.arity = arity
+        self.symbol = symbol
+        pass
+
+    @partial(jit, static_argnums=0)
+    def apply(self, x, y):
+        return self.operator(x, y)
+
+    def __call__(self, x, y):
+        return self.apply(x, y)
+
+    def tree_flatten(self):
+        children = ()
+        aux_data = {"operator": self.operator, "arity": self.arity, "symbol": self.symbol}
+        return children, aux_data
+
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        return cls(aux_data["operator"], aux_data["arity"], aux_data["symbol"])
 
 
 @jit
@@ -58,57 +83,19 @@ def greater(x, y):
     return 1.0 - lower(x, y)
 
 
+available_functions = {
+    "plus": JaxFunction(plus, 2, "+"),
+    "minus": JaxFunction(minus, 2, "-"),
+    "times": JaxFunction(times, 2, "*"),
+    "prot_div": JaxFunction(prot_div, 2, "/"),
+    "abs": JaxFunction(abs_val, 1, "|.|"),
+    "exp": JaxFunction(exp, 1, "exp"),
+    "sin": JaxFunction(sin, 1, "sin"),
+    "lower": JaxFunction(lower, 2, "<"),
+    "greater": JaxFunction(greater, 2, ">")
+}
+
+
 @jit
 def function_switch(idx, *operands):
-    return switch(idx, list(JaxFunction.existing_functions.values()), *operands)
-
-
-@register_pytree_node_class
-class JaxFunction:
-    existing_functions = {
-        "plus": plus,
-        "minus": minus,
-        "times": times,
-        "prot_div": prot_div,
-        "abs": abs_val,
-        "exp": exp,
-        "sin": sin,
-        "lower": lower,
-        "greater": greater
-    }
-
-    arities = {
-        "plus": 2,
-        "minus": 2,
-        "times": 2,
-        "prot_div": 2,
-        "abs": 1,
-        "exp": 1,
-        "sin": 1,
-        "lower": 2,
-        "greater": 2
-    }
-
-    def __init__(self, op) -> None:
-        self.operator = op
-        pass
-
-    @classmethod
-    def from_name(cls, name: str) -> JaxFunction:
-        return cls(cls.existing_functions[name])
-
-    @partial(jit, static_argnums=0)
-    def apply(self, x, y):
-        return self.operator(x, y)
-
-    def __call__(self, *args, **kwargs):
-        return self.apply(*args)
-
-    def tree_flatten(self):
-        children = ()
-        aux_data = {"operator": self.operator}
-        return children, aux_data
-
-    @classmethod
-    def tree_unflatten(cls, aux_data, children):
-        return cls(aux_data["operator"])
+    return switch(idx, list(available_functions.values()), *operands)
--- cgpax/utils.py
+++ cgpax/utils.py
@@ -4,7 +4,7 @@ import numpy as np
 import jax.numpy as jnp
 from jax import jit
 
-from cgpax.jax_functions import JaxFunction
+from cgpax.jax_functions import available_functions
 
 import pygraphviz as pgv
 
@@ -30,7 +30,7 @@ def __compute_active__(active: np.ndarray, x_genes: jnp.ndarray, y_genes: jnp.nd
     if not active[idx]:
         active[idx] = True
         __compute_active__(active, x_genes, y_genes, f_genes, n_in, int(x_genes[idx - n_in]))
-        _, arity = list(JaxFunction.arities.items())[f_genes[idx - n_in]]
+        arity = list(available_functions.values())[f_genes[idx - n_in]].arity
         if arity > 1:
             __compute_active__(active, x_genes, y_genes, f_genes, n_in, int(y_genes[idx - n_in]))
 
@@ -40,7 +40,7 @@ def readable_cgp_program_from_genome(genome: jnp.ndarray, config: dict) -> str:
     n_nodes = config["n_nodes"]
     x_genes, y_genes, f_genes, out_genes = jnp.split(genome, jnp.asarray([n_nodes, 2 * n_nodes, 3 * n_nodes]))
     active = compute_active(x_genes, y_genes, f_genes, out_genes, config)
-    function_names = list(JaxFunction.existing_functions.keys())
+    functions = list(available_functions.values())
     text_function = f"def program(inputs, buffer):\n" \
                     f"  buffer[{list(range(n_in))}] = inputs\n"
 
@@ -48,9 +48,9 @@ def readable_cgp_program_from_genome(genome: jnp.ndarray, config: dict) -> str:
     for buffer_idx in range(n_in, len(active)):
         if active[buffer_idx]:
             idx = buffer_idx - n_in
-            function_name = function_names[f_genes[idx]]
-            text_function += f"  buffer[{buffer_idx}] = {function_name}(buffer[{x_genes[idx]}]"
-            if JaxFunction.arities[function_name] > 1:
+            function = functions[f_genes[idx]]
+            text_function += f"  buffer[{buffer_idx}] = {function.symbol}(buffer[{x_genes[idx]}]"
+            if function.arity > 1:
                 text_function += f", buffer[{y_genes[idx]}]"
             text_function += ")\n"
 
@@ -61,15 +61,15 @@ def readable_cgp_program_from_genome(genome: jnp.ndarray, config: dict) -> str:
 
 def readable_lgp_program_from_genome(genome: jnp.ndarray, config: dict) -> str:
     lhs_genes, x_genes, y_genes, f_genes = jnp.split(genome, 4)
-    function_names = list(JaxFunction.existing_functions.keys())
+    functions = list(available_functions.values())
     text_function = f"def program(inputs, r):\n" \
                     f"  r[{list(range(config['n_in']))}] = inputs\n"
 
     # execution
     for row_idx in range(config["n_rows"]):
-        function_name = function_names[f_genes[row_idx]]
-        text_function += f"  r[{lhs_genes[row_idx] + config['n_in']}] = {function_name}(r[{x_genes[row_idx]}]"
-        if JaxFunction.arities[function_name] > 1:
+        function = functions[f_genes[row_idx]]
+        text_function += f"  r[{lhs_genes[row_idx] + config['n_in']}] = {function.symbol}(r[{x_genes[row_idx]}]"
+        if function.arity > 1:
             text_function += f", r[{y_genes[row_idx]}]"
         text_function += ")\n"
 
@@ -98,7 +98,7 @@ def lgp_graph_from_genome(genome: jnp.ndarray, config: dict, x_color: str = "blu
                           y_color: str = "orange") -> pgv.AGraph:
     lhs_genes, x_genes, y_genes, f_genes = jnp.split(genome, 4)
     lhs_genes += config['n_in']
-    function_names = list(JaxFunction.existing_functions.keys())
+    functions = list(available_functions.values())
     n_rows = len(lhs_genes)
     lhs_ids, x_ids, y_ids = [None] * n_rows, ["0"] * n_rows, ["0"] * n_rows
 
@@ -107,13 +107,13 @@ def lgp_graph_from_genome(genome: jnp.ndarray, config: dict, x_color: str = "blu
     missing_outputs = set(range(config["n_registers"] - config["n_out"], config["n_registers"]))
     for row_id in range(n_rows - 1, -1, -1):
         if lhs_ids[row_id] is not None or int(lhs_genes.at[row_id].get()) in missing_outputs:
-            function = function_names[f_genes.at[row_id].get()]
+            function = functions[f_genes.at[row_id].get()]
             x_register = x_genes.at[row_id].get()
             x_name = f"g_{row_id}" if x_register > config["n_in"] else f"i_{x_register}"
             x_ids[row_id] = x_name
             lhs_ids, x_ids, y_ids = __reassign_variables__(lhs_genes, x_genes, y_genes, lhs_ids, x_ids, y_ids,
                                                            row_id, x_genes.at[row_id].get(), x_name)
-            if JaxFunction.arities[function] > 1:
+            if function.arity > 1:
                 y_register = y_genes.at[row_id].get()
                 y_name = f"h_{row_id}" if y_register > config["n_in"] else f"i_{y_register}"
                 y_ids[row_id] = y_name
@@ -123,15 +123,15 @@ def lgp_graph_from_genome(genome: jnp.ndarray, config: dict, x_color: str = "blu
                 y_ids[row_id] = None
             if int(lhs_genes.at[row_id].get()) in missing_outputs:
                 missing_outputs.remove(int(lhs_genes.at[row_id].get()))
-                lhs_ids[row_id] = f"{function}_{row_id}"
-                graph.add_edge(f"{function}_{row_id}",
+                lhs_ids[row_id] = f"{function.symbol}_{row_id}"
+                graph.add_edge(f"{function.symbol}_{row_id}",
                                f'o_{config["n_out"] - (config["n_registers"] - lhs_genes.at[row_id].get())}')
 
     renaming_dict = {}
     for row_id in range(n_rows):
         if lhs_ids[row_id] is not None:
-            function = function_names[f_genes.at[row_id].get()]
-            lhs = f"{function}_{row_id}"
+            function = functions[f_genes.at[row_id].get()]
+            lhs = f"{function.symbol}_{row_id}"
             renaming_dict[lhs_ids[row_id]] = lhs
             x = x_ids[row_id] if x_ids[row_id].startswith("i_") else renaming_dict.get(x_ids[row_id], "0")
             if x == "0":
@@ -149,12 +149,15 @@ def cgp_graph_from_genome(genome: jnp.ndarray, config: dict, x_color: str = "blu
     n_nodes = config["n_nodes"]
     x_genes, y_genes, f_genes, out_genes = jnp.split(genome, jnp.asarray([n_nodes, 2 * n_nodes, 3 * n_nodes]))
     active = compute_active(x_genes, y_genes, f_genes, out_genes, config)
-    function_names = list(JaxFunction.existing_functions.keys())
+    functions = list(available_functions.values())
 
     graph = pgv.AGraph(directed=True)
     node_ids = [f"i_{n}" for n in range(n_in)]
+    arities = []
     for f_id, fx in enumerate(f_genes):
-        node_ids.append(f"{function_names[fx]}_{f_id + n_in}")
+        function = functions[fx]
+        node_ids.append(f"{function.symbol}_{f_id + n_in}")
+        arities.append(function.arity)
     for out_id, out_gene in enumerate(out_genes):
         graph.add_edge(node_ids[out_gene], f"o_{out_id}")
 
@@ -162,9 +165,9 @@ def cgp_graph_from_genome(genome: jnp.ndarray, config: dict, x_color: str = "blu
         if active[buffer_idx]:
             idx = buffer_idx - n_in
             current_node = node_ids[buffer_idx]
+            current_arity = arities[idx]
             graph.add_edge(node_ids[x_genes[idx]], current_node, color=x_color)
-            function_name = "prot_div" if current_node.startswith("prot_div") else current_node.split("_")[0]
-            if JaxFunction.arities[function_name] > 1:
+            if current_arity > 1:
                 graph.add_edge(node_ids[y_genes[idx]], current_node, color=y_color)
 
     return graph
