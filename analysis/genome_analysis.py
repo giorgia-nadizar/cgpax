1a4877c6c37728ab6e062e9584a309392d3523f7
giorgia-nadizar <giorgia.nadizar@gmail.com>
2023-09-11 12:25:36 +0000

    Expression added, proper episode termination, custom genome loading added.

--- analysis/genome_analysis.py
+++ analysis/genome_analysis.py
@@ -12,7 +12,7 @@ import cgpax.jax_encoding
 from jax import jit
 
 from cgpax.utils import readable_cgp_program_from_genome, cgp_graph_from_genome, readable_lgp_program_from_genome, \
-    lgp_graph_from_genome
+    lgp_graph_from_genome, cgp_expression_from_genome, lgp_expression_from_genome
 from cgpax.jax_encoding import genome_to_cgp_program, genome_to_lgp_program
 from cgpax.run_utils import __update_config_with_env_data__
 
@@ -27,6 +27,18 @@ def __write_readable_program__(genome: jnp.ndarray, config: dict, target_file: s
     else:
         with open(target_file, "w") as f:
             f.write(readable_program)
+
+
+def __write_expression__(genome: jnp.ndarray, config: dict, target_file: str = None):
+    if config["solver"] == "cgp":
+        readable_expression = cgp_expression_from_genome(genome, config)
+    else:
+        readable_expression = lgp_expression_from_genome(genome, config)
+    if target_file is None:
+        print(readable_expression)
+    else:
+        with open(target_file, "w") as f:
+            f.write(readable_expression)
 
 
 def __save_graph__(genome: jnp.ndarray, config: dict, file: str, input_color: str = None, output_color: str = None):
@@ -62,11 +74,17 @@ def __save_html_visualization__(genome: jnp.ndarray, config: dict, env: EpisodeW
     state = jit_env_reset(rng=rng)
     buffer = jnp.zeros(config["buffer_size"]) if config["solver"] == "cgp" else jnp.zeros(config["n_registers"])
     reward = 0
-    for _ in range(config["problem"]["episode_length"]):
+    for timestep in range(config["problem"]["episode_length"]):
         rollout.append(state)
         buffer, actions = jit_program(state.obs, buffer)
         state = jit_env_step(state, actions)
-        reward += state.reward
+        done = state.done
+        if done:
+            print(f"DONE [timestep {timestep}]")
+            if config["unhealthy_termination"]:
+                break
+        else:
+            reward += state.reward
 
     with open(file, "w") as f_episode:
         html_content = html.render(env.sys, [s.qp for s in rollout])
@@ -75,17 +93,15 @@ def __save_html_visualization__(genome: jnp.ndarray, config: dict, env: EpisodeW
     return reward
 
 
-def __load_last_genome__(base_path: str, seed: int) -> Tuple[jnp.ndarray, int]:
-    generation = 0
+def __load_genome__(base_path: str, seed: int, generation: int = "last") -> Tuple[jnp.ndarray, int]:
+    generations = []
     for gene_file in os.listdir(base_path):
         if gene_file == "config.yaml":
             continue
-        current_generation = int(gene_file.split("_")[1])
-        if current_generation > generation:
-            generation = current_generation
-    genes = jnp.load(f"{base_path}/{seed}_{generation}_best_genome.npy",
-                     allow_pickle=True).astype(int)
-    return genes, generation
+        generations.append(int(gene_file.split("_")[1]))
+
+    gen = max(generations) if generation < 0 else min(generations, key=lambda x: abs(generation - x))
+    return jnp.load(f"{base_path}/{seed}_{gen}_best_genome.npy", allow_pickle=True).astype(int), gen
 
 
 if __name__ == '__main__':
@@ -102,13 +118,14 @@ if __name__ == '__main__':
                 continue
 
             cfg = cgpax.get_config(f"{base_path}/config.yaml")
-            genes, generation = __load_last_genome__(base_path, seed)
+            genes, generation = __load_genome__(base_path, seed, analysis_config["generation"])
 
             environment = envs.get_environment(env_name=cfg["problem"]["environment"])
             environment = EpisodeWrapper(environment, episode_length=cfg["problem"]["episode_length"], action_repeat=1)
             __update_config_with_env_data__(cfg, environment)
 
             __write_readable_program__(genes, cfg, f"{target_dir}/{folder}.txt")
+            __write_expression__(genes, cfg, f"{target_dir}/{folder}_expression.txt")
             __save_graph__(genes, cfg, f"{target_dir}/{folder}.png", analysis_config["input_color"],
                            analysis_config["output_color"])
 
