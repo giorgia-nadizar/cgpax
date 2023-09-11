c576786ec2a2dbf1fbfde358389252226a00fb31
giorgia-nadizar <giorgia.nadizar@gmail.com>
2023-09-11 11:21:48 +0000

    Weighted graph GP encoding added.

--- cgpax/jax_encoding.py
+++ cgpax/jax_encoding.py
@@ -15,32 +15,36 @@ def __offset_copy__(idx: int, carry: Tuple[int, jnp.ndarray, jnp.ndarray]) -> Tu
 
 
 @jit
-def __update_buffer__(buffer_idx: int, carry: Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]) -> Tuple[
-    jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
-    x_genes, y_genes, f_genes, buffer = carry
+def __update_buffer__(buffer_idx: int,
+                      carry: Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]) -> Tuple[
+    jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+    x_genes, y_genes, f_genes, weights, buffer = carry
     n_in = len(buffer) - len(x_genes)
     idx = buffer_idx - n_in
     f_idx = f_genes.at[idx].get()
+    weight = weights.at[idx].get()
     x_arg = buffer.at[x_genes.at[idx].get()].get()
     y_arg = buffer.at[y_genes.at[idx].get()].get()
 
-    buffer = buffer.at[buffer_idx].set(function_switch(f_idx, x_arg, y_arg))
-    return x_genes, y_genes, f_genes, buffer
+    buffer = buffer.at[buffer_idx].set(function_switch(f_idx, x_arg, y_arg) * weight)
+    return x_genes, y_genes, f_genes, weights, buffer
 
 
 @jit
 def __update_register__(row_idx: int,
-                        carry: Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, int, jnp.ndarray]) -> Tuple[
-    jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, int, jnp.ndarray]:
-    lhs_genes, x_genes, y_genes, f_genes, n_in, register = carry
+                        carry: Tuple[
+                            jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, int, jnp.ndarray]) -> \
+        Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, int, jnp.ndarray]:
+    lhs_genes, x_genes, y_genes, f_genes, weights, n_in, register = carry
     lhs_idx = lhs_genes.at[row_idx].get() + n_in
     f_idx = f_genes.at[row_idx].get()
+    weight = weights.at[row_idx].get()
     x_idx = x_genes.at[row_idx].get()
     x_arg = register.at[x_idx].get()
     y_idx = y_genes.at[row_idx].get()
     y_arg = register.at[y_idx].get()
-    register = register.at[lhs_idx].set(function_switch(f_idx, x_arg, y_arg))
-    return lhs_genes, x_genes, y_genes, f_genes, n_in, register
+    register = register.at[lhs_idx].set(function_switch(f_idx, x_arg, y_arg) * weight)
+    return lhs_genes, x_genes, y_genes, f_genes, weights, n_in, register
 
 
 def genome_to_cgp_program(genome: jnp.ndarray, config: dict,
@@ -48,9 +52,15 @@ def genome_to_cgp_program(genome: jnp.ndarray, config: dict,
     n_in_env = config["n_in_env"]
     n_const = config["n_constants"]
     n_in = config["n_in"]
+    n_out = config["n_out"]
     n_nodes = config["n_nodes"]
 
-    x_genes, y_genes, f_genes, out_genes = jnp.split(genome, [n_nodes, 2 * n_nodes, 3 * n_nodes])
+    x_genes, y_genes, f_genes, out_genes, weights = jnp.split(genome,
+                                                              [n_nodes, 2 * n_nodes, 3 * n_nodes, 3 * n_nodes + n_out])
+    x_genes = x_genes.astype(int)
+    y_genes = y_genes.astype(int)
+    f_genes = f_genes.astype(int)
+    out_genes = out_genes.astype(int)
 
     def program(inputs: jnp.ndarray, buffer: jnp.ndarray) -> (jnp.ndarray, jnp.ndarray):
         # copy actual inputs
@@ -58,7 +68,8 @@ def genome_to_cgp_program(genome: jnp.ndarray, config: dict,
         # copy constants
         _, _, buffer = fori_loop(0, n_const, __offset_copy__, (n_in_env, constants, buffer))
 
-        _, _, _, buffer = fori_loop(n_in, len(buffer), __update_buffer__, (x_genes, y_genes, f_genes, buffer))
+        _, _, _, _, buffer = fori_loop(n_in, len(buffer), __update_buffer__,
+                                       (x_genes, y_genes, f_genes, weights, buffer))
         outputs = jnp.take(buffer, out_genes)
         bounded_outputs = outputs_wrapper(outputs)
 
@@ -77,7 +88,11 @@ def genome_to_lgp_program(genome: jnp.ndarray, config: dict,
     n_registers = config["n_registers"]
     output_positions = jnp.arange(start=n_registers - n_out, stop=n_registers)
 
-    lhs_genes, x_genes, y_genes, f_genes = jnp.split(genome, 4)
+    lhs_genes, x_genes, y_genes, f_genes, weights = jnp.split(genome, 5)
+    lhs_genes = lhs_genes.astype(int)
+    x_genes = x_genes.astype(int)
+    y_genes = y_genes.astype(int)
+    f_genes = f_genes.astype(int)
 
     def program(inputs: jnp.ndarray, register: jnp.ndarray) -> (jnp.ndarray, jnp.ndarray):
         register = jnp.zeros(n_registers)
@@ -85,8 +100,8 @@ def genome_to_lgp_program(genome: jnp.ndarray, config: dict,
         _, _, register = fori_loop(0, n_in_env, __offset_copy__, (0, inputs, register))
         # copy constant inputs
         _, _, register = fori_loop(0, n_const, __offset_copy__, (n_in_env, constants, register))
-        _, _, _, _, _, register = fori_loop(0, n_rows, __update_register__,
-                                            (lhs_genes, x_genes, y_genes, f_genes, n_in, register))
+        _, _, _, _, _, _, register = fori_loop(0, n_rows, __update_register__,
+                                               (lhs_genes, x_genes, y_genes, f_genes, weights, n_in, register))
         outputs = jnp.take(register, output_positions)
         bounded_outputs = outputs_wrapper(outputs)
 
