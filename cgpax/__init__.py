9691d87cc028b525380a0662d830ca33710ee537
giorgia-nadizar <giorgia.nadizar@gmail.com>
2023-09-26 11:17:51 +0000

    Typing fixed.

--- cgpax/__init__.py
+++ cgpax/__init__.py
@@ -1,9 +1,10 @@
 from pathlib import Path
+from typing import Dict
 
 import yaml as yaml
 
 
-def get_config(filename: str) -> dict:
+def get_config(filename: str) -> Dict:
     config = yaml.safe_load(Path(filename).read_text())
     flat_config = {}
     for key in config:
