cc6857e11580d7c82b4aaacd77c0c012cee6b7cf
giorgia-nadizar <giorgia.nadizar@gmail.com>
2023-10-03 14:26:45 +0000

    Methods renamed according to convention.

--- cgpax/jax_selection.py
+++ cgpax/jax_selection.py
@@ -27,8 +27,8 @@ def fp_selection(genomes: jnp.ndarray, fitness_values: jnp.ndarray, rnd_key: ran
 
 def tournament_selection(genomes: jnp.ndarray, fitness_values: jnp.ndarray, rnd_key: random.PRNGKey,
                          n_elites: int, tour_size: int) -> jnp.ndarray:
-    def single_tournament(sample_key: random.PRNGKey, genomes: jnp.ndarray, fitness_values: jnp.ndarray,
-                          tour_size: int) -> jnp.ndarray:
+    def _tournament(sample_key: random.PRNGKey, genomes: jnp.ndarray, fitness_values: jnp.ndarray,
+                    tour_size: int) -> jnp.ndarray:
         indexes = random.choice(sample_key, jnp.arange(start=0, stop=len(genomes)), shape=[tour_size], replace=True)
         mask = jnp.zeros_like(fitness_values)
         mask = mask.at[indexes].set(1)
@@ -37,7 +37,7 @@ def tournament_selection(genomes: jnp.ndarray, fitness_values: jnp.ndarray, rnd_
         return best_genome
 
     sample_keys = random.split(rnd_key, n_elites)
-    partial_single_tournament = partial(single_tournament, genomes=genomes, fitness_values=fitness_values,
+    partial_single_tournament = partial(_tournament, genomes=genomes, fitness_values=fitness_values,
                                         tour_size=tour_size)
     vmap_tournament = vmap(partial_single_tournament)
     return vmap_tournament(sample_key=sample_keys)
