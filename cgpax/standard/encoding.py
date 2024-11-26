c68f4154ec1ade4dbdd86f562db53127f92f1ff1
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-11-26 18:16:29 +0100

    encoding for boolean functions implemented

--- cgpax/standard/encoding.py
+++ cgpax/standard/encoding.py
@@ -4,12 +4,12 @@ import jax.numpy as jnp
 from jax import jit
 from jax.lax import fori_loop
 
-from cgpax.functions import control_function_switch, constants
+from cgpax.functions import control_function_switch, constants, boolean_function_switch
 
 
 @jit
-def _update_buffer(buffer_idx: int,
-                   carry: Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]) -> Tuple[
+def _update_buffer_control(buffer_idx: int,
+                           carry: Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]) -> Tuple[
     jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
     x_genes, y_genes, f_genes, buffer = carry
     n_in = len(buffer) - len(x_genes)
@@ -23,7 +23,22 @@ def _update_buffer(buffer_idx: int,
 
 
 @jit
-def _update_register(row_idx: int, carry: Tuple[
+def _update_buffer_boolean(buffer_idx: int,
+                           carry: Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]) -> Tuple[
+    jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+    x_genes, y_genes, f_genes, buffer = carry
+    n_in = len(buffer) - len(x_genes)
+    idx = buffer_idx - n_in
+    f_idx = f_genes.at[idx].get()
+    x_arg = buffer.at[x_genes.at[idx].get()].get()
+    y_arg = buffer.at[y_genes.at[idx].get()].get()
+
+    buffer = buffer.at[buffer_idx].set(boolean_function_switch(f_idx, x_arg, y_arg))
+    return x_genes, y_genes, f_genes, buffer
+
+
+@jit
+def _update_register_control(row_idx: int, carry: Tuple[
     jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, int, jnp.ndarray]) -> \
         Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, int, jnp.ndarray]:
     lhs_genes, x_genes, y_genes, f_genes, n_in, register = carry
@@ -37,11 +52,27 @@ def _update_register(row_idx: int, carry: Tuple[
     return lhs_genes, x_genes, y_genes, f_genes, n_in, register
 
 
+@jit
+def _update_register_boolean(row_idx: int, carry: Tuple[
+    jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, int, jnp.ndarray]) -> \
+        Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, int, jnp.ndarray]:
+    lhs_genes, x_genes, y_genes, f_genes, n_in, register = carry
+    lhs_idx = lhs_genes.at[row_idx].get() + n_in
+    f_idx = f_genes.at[row_idx].get()
+    x_idx = x_genes.at[row_idx].get()
+    x_arg = register.at[x_idx].get()
+    y_idx = y_genes.at[row_idx].get()
+    y_arg = register.at[y_idx].get()
+    register = register.at[lhs_idx].set(boolean_function_switch(f_idx, x_arg, y_arg))
+    return lhs_genes, x_genes, y_genes, f_genes, n_in, register
+
+
 def genome_to_cgp_program(genome: jnp.ndarray, config: Dict,
-                          outputs_wrapper: Callable[[jnp.ndarray], jnp.ndarray] = jnp.tanh) -> Callable:
+                          outputs_wrapper: Callable[[jnp.ndarray], jnp.ndarray] = jnp.tanh,
+                          buffer_update_fn: Callable = _update_buffer_control
+                          ) -> Callable:
     n_const = config["n_constants"]
     n_in = config["n_in"]
-    n_out = config["n_out"]
     n_nodes = config["n_nodes"]
 
     x_genes, y_genes, f_genes, out_genes = jnp.split(genome, [n_nodes, 2 * n_nodes, 3 * n_nodes])
@@ -52,8 +83,8 @@ def genome_to_cgp_program(genome: jnp.ndarray, config: Dict,
 
     def _program(inputs: jnp.ndarray, buffer: jnp.ndarray) -> (jnp.ndarray, jnp.ndarray):
         buffer = jnp.concatenate([inputs, constants[:n_const], buffer[n_in:len(buffer)]])
-        _, _, _, buffer = fori_loop(n_in, len(buffer), _update_buffer,
-                                       (x_genes, y_genes, f_genes, buffer))
+        _, _, _, buffer = fori_loop(n_in, len(buffer), buffer_update_fn,
+                                    (x_genes, y_genes, f_genes, buffer))
         outputs = jnp.take(buffer, out_genes)
         bounded_outputs = outputs_wrapper(outputs)
 
@@ -63,7 +94,8 @@ def genome_to_cgp_program(genome: jnp.ndarray, config: Dict,
 
 
 def genome_to_lgp_program(genome: jnp.ndarray, config: Dict,
-                          outputs_wrapper: Callable[[jnp.ndarray], jnp.ndarray] = jnp.tanh) -> Callable:
+                          outputs_wrapper: Callable[[jnp.ndarray], jnp.ndarray] = jnp.tanh,
+                          register_update_fn: Callable = _update_register_control) -> Callable:
     n_const = config["n_constants"]
     n_in = config["n_in"]
     n_out = config["n_out"]
@@ -80,7 +112,7 @@ def genome_to_lgp_program(genome: jnp.ndarray, config: Dict,
     def _program(inputs: jnp.ndarray, register: jnp.ndarray) -> (jnp.ndarray, jnp.ndarray):
         register = jnp.zeros(n_registers)
         register = jnp.concatenate([inputs, constants[:n_const], register[n_in:len(register)]])
-        _, _, _, _, _, register = fori_loop(0, n_rows, _update_register,
+        _, _, _, _, _, register = fori_loop(0, n_rows, register_update_fn,
                                             (lhs_genes, x_genes, y_genes, f_genes, n_in, register))
         outputs = jnp.take(register, output_positions)
         bounded_outputs = outputs_wrapper(outputs)
