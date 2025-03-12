d2a9f02e9ddf2f1f72a09abdfd24e4a6000db3a0
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2025-03-12 09:08:03 +0100

    bug fixed

--- cgpax/run_utils.py
+++ cgpax/run_utils.py
@@ -93,8 +93,7 @@ def update_config_with_data(config: Dict, input_space_size: int, output_space_si
 
 def update_config_with_env_data(config: Dict, env) -> None:
     action_size = env.action_space.n if hasattr(env, "action_space") else env.action_size
-    observation_size = env.observation_space.shape[0] if isinstance(env.observation_space,
-                                                                    Box) else env.observation_size
+    observation_size = env.observation_space.shape[0] if hasattr(env, "observation_space") else env.observation_size
     update_config_with_data(config, observation_size, action_size)
 
 
