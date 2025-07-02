0de44bd16284a9b51230c6be1d08d4bfa6311207
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2025-07-02 10:02:17 +0200

    simple lexicase selection

--- cgpax/selection.py
+++ cgpax/selection.py
@@ -41,3 +41,39 @@ def tournament_selection(genomes: jnp.ndarray, fitness_values: jnp.ndarray, rnd_
                                         tour_size=tour_size)
     vmap_tournament = vmap(partial_single_tournament)
     return vmap_tournament(sample_key=sample_keys)
+
+
+def lexicase_selection(genomes: jnp.ndarray, fitness_values: jnp.ndarray, rnd_key: random.PRNGKey,
+                       n_elites: int):
+    selected_ids = []
+
+    # Get the number of test cases
+    num_cases = fitness_values.shape[1]
+
+    while len(selected_ids) < n_elites:
+
+        candidates_ids = [x for x in range(len(genomes)) if x not in selected_ids]
+        for selected_id in selected_ids:
+            fitness_values = fitness_values.at[selected_id].set(jnp.ones(num_cases) * -jnp.inf)
+
+        # Create a shuffled list of case indices
+        case_indices = jnp.arange(num_cases)
+        rnd_key, shuffle_key = random.split(rnd_key, 2)
+        case_indices = random.permutation(shuffle_key, case_indices)
+
+        # Iterate through the shuffled test cases
+        for case_idx in case_indices:
+            # If only one candidate remains, return it
+            if len(candidates_ids) == 1:
+                selected_ids.append(candidates_ids[0])
+                break
+
+            # Extract the error scores for the current case across candidates
+            case_fitness_values = fitness_values[:, case_idx]
+
+            # Filter candidates to only include those with the max reward
+            candidates_ids = jnp.argwhere(case_fitness_values == jnp.max(case_fitness_values))
+
+        selected_ids.extend(candidates_ids)
+
+    return genomes[selected_ids[0:n_elites]]
