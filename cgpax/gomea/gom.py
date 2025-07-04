3f12f93e7c940b8be3e2bf8dc039c6c775b83eca
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2025-07-04 10:22:24 +0200

    measurements on evaluations rather than iterations

--- cgpax/gomea/gom.py
+++ cgpax/gomea/gom.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 from functools import partial
 
 import jax.numpy as jnp
-from typing import List, Tuple, Callable, Union
+from typing import List, Tuple, Callable, Union, Dict
 
 from jax import random, jit
 import random as rnd
@@ -23,17 +23,16 @@ def parallel_gom(
         track_fitnesses: bool = False,
         intermediate_prints: bool = False,
         test_eval_fn: Callable[[jnp.ndarray, random.PRNGKey], float] = None,
-) -> Union[Tuple[jnp.ndarray, jnp.ndarray], Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray], Tuple[
-    jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]]:
+) -> Union[Tuple[jnp.ndarray, jnp.ndarray], Tuple[jnp.ndarray, jnp.ndarray, List[Dict]]]:
     mutation_fn = partial(_gom_mutate, donors=donors)
     array_fos = [jnp.asarray(f) for f in fos]
     shuffled_fos = [rnd.sample(array_fos, len(array_fos)) for _ in donors]
 
     genotypes = deepcopy(donors)
-    fitnesses_history = []
-    test_accuracies_history = []
+    history_dicts = []
 
     for f_idx in range(len(array_fos)):
+
         rnd_key, *mutate_keys = random.split(rnd_key, len(donors) + 1)
         offspring_genotypes = jnp.asarray([mutation_fn(genotype, shuffled_fos[g_idx][f_idx], mutate_keys[g_idx])
                                            for g_idx, genotype in enumerate(genotypes)])
@@ -43,21 +42,23 @@ def parallel_gom(
 
         genotypes = jnp.where((offspring_fitnesses > fitnesses)[:, None], offspring_genotypes, genotypes)
         fitnesses = jnp.where(offspring_fitnesses > fitnesses, offspring_fitnesses, fitnesses)
-        fitnesses_history.append(jnp.max(fitnesses))
+        current_iteration_dict = {
+            "evaluation": f_idx * len(genotypes),
+            "max_fitness": jnp.max(fitnesses),
+        }
 
         if intermediate_prints:
-            print(f"\t {f_idx} \t FITNESS: {jnp.max(fitnesses)}")
+            print(f"\t {f_idx * len(genotypes)} \t FITNESS: {jnp.max(fitnesses)}")
 
         if test_eval_fn is not None:
             best_individual = genotypes[jnp.argmax(fitnesses)]
             rnd_key, test_key = random.split(rnd_key, 2)
             best_test_accuracy = test_eval_fn(best_individual, test_key)
-            test_accuracies_history.append(best_test_accuracy)
+            current_iteration_dict["test_accuracy"] = best_test_accuracy
+
+        history_dicts.append(current_iteration_dict)
 
     if track_fitnesses:
-        if test_eval_fn is None:
-            return genotypes, fitnesses, jnp.asarray(fitnesses_history)
-        else:
-            return genotypes, fitnesses, jnp.asarray(fitnesses_history), jnp.asarray(test_accuracies_history)
+        return genotypes, fitnesses, history_dicts
     else:
         return genotypes, fitnesses
