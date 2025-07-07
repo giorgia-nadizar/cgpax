40c0b2a4ed02510edba23c43bc4d37683aa27727
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2025-07-07 11:17:00 +0200

    combined regression datasets

--- datasets/dataset_maker.py
+++ datasets/dataset_maker.py
@@ -1,4 +1,33 @@
+import jax
 import jax.numpy as jnp
+from typing import Callable, List
+
+
+def nguyen_9(x: jnp.ndarray) -> jnp.ndarray:
+    return jnp.sin(x[:, 0]) + jnp.sin(x[:, 1] ** 2)
+
+
+def nguyen_10(x: jnp.ndarray) -> jnp.ndarray:
+    return 2 * jnp.sin(x[:, 0]) * jnp.cos(x[:, 1])
+
+
+def nguyen_12(x: jnp.ndarray) -> jnp.ndarray:
+    return jnp.sin(x[:, 0]) ** 4 - jnp.sin(x[:, 0]) ** 3 + 0.5 * jnp.sin(x[:, 1]) ** 2 - jnp.sin(x[:, 1])
+
+
+def combine_regression_functions(x: jnp.ndarray,
+                                 regression_functions: List[Callable[[jnp.ndarray], jnp.ndarray]],
+                                 file_path: str):
+    y = jnp.hstack([f(x).reshape(-1, 1) for f in regression_functions])
+    jnp.save(f"{file_path}_x.npy", x)
+    jnp.save(f"{file_path}_y.npy", y)
+
+
+def nguyen_9_12():
+    file_path = "nguyen_9_12"
+    key = jax.random.PRNGKey(0)
+    x = jax.random.uniform(key, shape=(20, 2), minval=0.0, maxval=1.0)
+    combine_regression_functions(x, [nguyen_9, nguyen_12], file_path)
 
 
 def two_bits_input() -> jnp.ndarray:
@@ -14,7 +43,7 @@ def two_bits_input() -> jnp.ndarray:
 # 2-bit adder
 def two_bits_adder():
     file_path = "two_bit_adder"
-    jnp.save(f"datasets/{file_path}_x.npy", two_bits_input())
+    jnp.save(f"{file_path}_x.npy", two_bits_input())
 
     outputs_list = [
         [0, 0, 0],
@@ -35,13 +64,13 @@ def two_bits_adder():
         [1, 1, 0],
     ]
     outputs = jnp.asarray(outputs_list)
-    jnp.save(f"datasets/{file_path}_y.npy", outputs)
+    jnp.save(f"{file_path}_y.npy", outputs)
 
 
 def two_bit_multiplier():
     file_path = "two_bit_multiplier"
 
-    jnp.save(f"datasets/{file_path}_x.npy", two_bits_input())
+    jnp.save(f"{file_path}_x.npy", two_bits_input())
 
     outputs_list = [
         [0, 0, 0, 0],
@@ -62,8 +91,9 @@ def two_bit_multiplier():
         [1, 0, 0, 1],
     ]
     outputs = jnp.asarray(outputs_list)
-    jnp.save(f"datasets/{file_path}_y.npy", outputs)
+    jnp.save(f"{file_path}_y.npy", outputs)
 
 
 if __name__ == '__main__':
-    two_bit_multiplier()
+    # two_bit_multiplier()
+    nguyen_9_12()
