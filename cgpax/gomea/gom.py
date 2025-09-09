2f1d569fd4128ee5ec09f30c4b28b0bc52182828
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2025-09-09 11:59:17 +0200

    diversity in parallel gom made with jax

--- cgpax/gomea/gom.py
+++ cgpax/gomea/gom.py
@@ -4,8 +4,12 @@ from functools import partial
 import jax.numpy as jnp
 from typing import List, Tuple, Callable, Union, Dict
 
-from jax import random, jit
+from brax.experimental.composer.envs.sa_descs import new_desc_name
+from jax import random, jit, lax
 import random as rnd
+
+from nbformat.current import current_nbformat_minor, current_nbformat_module
+from zstandard.backend_cffi import new_nonzero
 
 
 @jit
@@ -43,24 +47,30 @@ def parallel_gom(
         if not diversity_preservation:
             genotypes = jnp.where(better_offspring[:, None], offspring_genotypes, genotypes)
             fitnesses = jnp.where(better_offspring, offspring_fitnesses, fitnesses)
+            unique_ratio = len(jnp.unique(genotypes.astype(int), axis=0)) / len(genotypes)
         else:
-            previous_unique_ratio = len(jnp.unique(genotypes.astype(int), axis=0)) / len(genotypes)
-            diversity_preservation_mask = jnp.zeros_like(better_offspring, dtype=int)
-            for g_idx in range(len(diversity_preservation_mask)):
-                if not better_offspring[g_idx]:
-                    continue
-                tmp_div_mask = diversity_preservation_mask.at[g_idx].set(1)
+
+            def _diversity_insertion(g_idx: int, carry: Tuple[jnp.ndarray, int]):
+                diversity_mask, previous_n_uniques = carry
+                tmp_div_mask = diversity_mask.at[g_idx].set(1)
                 tmp_filter_mask = jnp.logical_and(better_offspring, tmp_div_mask)
                 tmp_genotypes = jnp.where(tmp_filter_mask[:, None], offspring_genotypes, genotypes)
-                new_unique_ratio = len(jnp.unique(tmp_genotypes.astype(int), axis=0)) / len(tmp_genotypes)
-                # if the insertion of the current individual does not decrease uniqueness, proceed
-                if new_unique_ratio >= previous_unique_ratio:
-                    diversity_preservation_mask = tmp_div_mask
-                    genotypes = tmp_genotypes
+                new_n_uniques = jnp.unique(tmp_genotypes.astype(jnp.int32), axis=0, size=genotypes.shape[0]).shape[0]
+                keep_this_individual = new_n_uniques >= previous_n_uniques
+                diversity_mask = diversity_mask.at[g_idx].set(keep_this_individual)
+                updated_n_uniques = (keep_this_individual * new_n_uniques +
+                                     (1 - keep_this_individual) * previous_n_uniques)
+                return diversity_mask, updated_n_uniques
+
+            diversity_preservation_mask, n_uniques = lax.fori_loop(0, len(fitnesses), _diversity_insertion,
+                                                                   (jnp.zeros_like(better_offspring, dtype=int),
+                                                                    len(jnp.unique(genotypes.astype(int), axis=0))))
+            unique_ratio = n_uniques / len(genotypes)
+            genotypes = jnp.where(jnp.logical_and(better_offspring, diversity_preservation_mask)[:, None],
+                                  offspring_genotypes, genotypes)
             fitnesses = jnp.where(jnp.logical_and(better_offspring, diversity_preservation_mask),
                                   offspring_fitnesses, fitnesses)
 
-        unique_ratio = len(jnp.unique(genotypes.astype(int), axis=0)) / len(genotypes)
         current_iteration_dict = {
             "evaluation": f_idx * len(genotypes),
             "max_fitness": jnp.max(fitnesses),
