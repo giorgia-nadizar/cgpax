5d0760b718517fa1043168894663efc49a811065
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2025-10-09 16:51:15 +0200

    script updated

--- scripts/parallel_runner.sh
+++ scripts/parallel_runner.sh
@@ -9,7 +9,7 @@ byobu send-keys -t $SESSION:base "echo 'GPAX run session started'" C-m
 FOSS=("U" "RT" "LT" "LT1")
 
 # Loop over seeds
-for SEED in {1..9}; do
+for SEED in {0..9}; do
   # Loop over FOS modes
   for FOS in "${FOSS[@]}"; do
     WIN_NAME="seed_${SEED}_${FOS}"
@@ -27,7 +27,7 @@ for SEED in {1..9}; do
 done
 
 # Loop over seeds
-for SEED in {1..9}; do
+for SEED in {0..9}; do
   WIN_NAME="seed_$SEED"
 
   # Create a new window
