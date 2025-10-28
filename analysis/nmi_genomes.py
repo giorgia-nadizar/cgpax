1fe5d68d835e65e8f73747998a9eeec07d692406
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2025-10-28 11:39:52 +0100

    data analysis

--- analysis/nmi_genomes.py
+++ analysis/nmi_genomes.py
@@ -20,36 +20,44 @@ def column_entropy(col):
 
 
 if __name__ == '__main__':
-    seed = 0
-    environment = "nguyen_9_12_dense"
+    seed = 5
+    environment = "feynman_13"
     pop_type = "_large_pop"
     solver = "lgp"
+    environments = {
+        # "regression": ["feynman_13", "feynman_43", "feynman_13_61"],
+        "control": ["inverted_double_pendulum", "reacher", "walker2d"]
+    }
+    # for environment in :
+    for env_type in environments.keys():
+        for environment in environments[env_type]:
+            for solver in ["lgp", "cgp"]:
+                print(environment, solver)
+                base_path = f"../results/{env_type}/gomea_{solver}_{environment}_LT1_{seed}_fi{pop_type}"
+                config = yaml.safe_load(Path(f"{base_path}/config.yml").read_text())
+                genomes = jnp.load(Path(f"{base_path}/genomes.npy"))
 
-    base_path = f"../results/gomea_{solver}_{environment}_LT_{seed}_fi{pop_type}"
-    config = yaml.safe_load(Path(f"{base_path}/config.yml").read_text())
-    genomes = jnp.load(Path(f"{base_path}/genomes.npy"))
+                # compute genomes activity histogram
+                active_genome_compute_fn = partial(compute_active_genome, config=config)
+                active_genomes = jnp.asarray([active_genome_compute_fn(g) for g in genomes])
+                activity_count = jnp.sum(active_genomes, axis=0)
+                jnp.save(f"{base_path}/activity_count.npy", activity_count)
+                print("\tgenomes activity")
 
-    # compute genomes activity histogram
-    active_genome_compute_fn = partial(compute_active_genome, config=config)
-    active_genomes = jnp.asarray([active_genome_compute_fn(g) for g in genomes])
-    activity_count = jnp.sum(active_genomes, axis=0)
-    jnp.save(f"{base_path}/activity_count.npy", activity_count)
-    print("genomes activity")
+                # compute unique genes
+                entropies = jnp.asarray([column_entropy(genomes[:, col]) for col in range(genomes.shape[1])])
+                jnp.save(Path(f"{base_path}/unique_genes.npy"), entropies)
+                print("\tgenomes uniqueness")
 
-    # compute unique genes
-    entropies = jnp.asarray([column_entropy(genomes[:, col]) for col in range(genomes.shape[1])])
-    jnp.save(Path(f"{base_path}/unique_genes.npy"), entropies)
-    print("genomes uniqueness")
-
-    # compute nmi matrix
-    genome_mask, mutation_mask = compute_masks(config)
-    genome_transformation_function = compute_genome_transformation_function(config)
-    rnd_key = random.PRNGKey(0)
-    init_genomes = individual.generate_population(pop_size=config["n_individuals"],
-                                                  genome_mask=genome_mask, rnd_key=rnd_key,
-                                                  genome_transformation_function=genome_transformation_function)
-    # _, bias_matrix = _compute_normalized_mutual_information_matrix(init_genomes, config, None)
-    bias_matrix = jnp.ones((len(genomes[0]), len(genomes[0])))
-    nmi_matrix, _ = _compute_normalized_mutual_information_matrix(genomes, config, bias_matrix)
-    jnp.save(f"{base_path}/nmi_matrix.npy", nmi_matrix)
-    print("nmi matrix")
+                # compute nmi matrix
+                genome_mask, mutation_mask = compute_masks(config)
+                genome_transformation_function = compute_genome_transformation_function(config)
+                rnd_key = random.PRNGKey(0)
+                init_genomes = individual.generate_population(pop_size=config["n_individuals"],
+                                                              genome_mask=genome_mask, rnd_key=rnd_key,
+                                                              genome_transformation_function=genome_transformation_function)
+                # _, bias_matrix = _compute_normalized_mutual_information_matrix(init_genomes, config, None)
+                bias_matrix = jnp.ones((len(genomes[0]), len(genomes[0])))
+                nmi_matrix, _ = _compute_normalized_mutual_information_matrix(genomes, config, bias_matrix)
+                jnp.save(f"{base_path}/nmi_matrix_biased.npy", nmi_matrix)
+                print("\tnmi matrix")
